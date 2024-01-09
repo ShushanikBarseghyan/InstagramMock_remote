0281dbfe6d22b9358e8978451e97013c50baf5eb
Shushanik <barseghyan.shushan@gmail.com>
2024-01-09 22:37:45 +0400

    Initial Commit

---
+++ InstagramMock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstagramMock
+//
+//  Created by Shushan Barseghyan on 09.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
