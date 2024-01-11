172defeb65f458a3379f79c02229d54ff200ceac
Shushanik <barseghyan.shushan@gmail.com>
2024-01-11 13:42:03 +0400

    TabBiew, SearchView, Feed, ProfileView

---
+++ InstagramMock/New Group2/New Group/New Group/Core/Components/UserStatView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatView.swift
+//  InstagramMock
+//
+//  Created by Shushan Barseghyan on 09.01.24.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatView()
+}
---
+++ InstagramMock/New Group2/New Group/New Group/Core/Feed/FeedCellView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCellView.swift
+//  InstagramMock
+//
+//  Created by Shushan Barseghyan on 10.01.24.
+//
+
+import SwiftUI
+
+struct FeedCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCellView()
+}
