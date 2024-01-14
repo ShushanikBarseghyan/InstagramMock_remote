079fd61075b99c1dced691a84a6c8321ae7e7f6f
Shushanik <barseghyan.shushan@gmail.com>
2024-01-14 23:32:37 +0400

    login and sign up views

---
+++ InstagramMock/New Group2/New Group/New Group/Core/Authentication/CompleteSignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  CompleteSignUpView.swift
+//  InstagramMock
+//
+//  Created by Shushan Barseghyan on 14.01.24.
+//
+
+import SwiftUI
+
+struct CompleteSignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompleteSignUpView()
+}
